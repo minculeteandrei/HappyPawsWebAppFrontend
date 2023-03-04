83a1b778a21f412cfb511a372f6af70231f90403
Andrei <minculeteandrei@gmail.com>
2023-03-04 21:37:54 +0200

    added modules as pages and common-modules. Added navbar

---
+++ src/common-modules/common-modules.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainNavbarComponent } from './components/main-navbar/main-navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+
+
+@NgModule({
+  declarations: [
+    MainNavbarComponent
+  ],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ]
+})
+export class CommonModulesModule { }
---
+++ src/common-modules/components/main-navbar/main-navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-main-navbar',
+  templateUrl: './main-navbar.component.html',
+  styleUrls: ['./main-navbar.component.scss']
+})
+export class MainNavbarComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
